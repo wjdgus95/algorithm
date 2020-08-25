550abe492c05d81647e293aa21e26983320e5d04
wjdgus95 <wjdguswjdal@naver.com>
2020-08-25 10:24:19 +0900

    Boj4949

--- src/al/ex15.java
+++ src/al/ex15.java
@@ -2,45 +2,54 @@ package al;
 
 import java.io.*;
 import java.util.*;
+
 public class ex15 {
 
-	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
-		BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-		int n=Integer.parseInt(bf.readLine());
-		
-		while(n-- >0) {
-			boolean isVPS= true;
-			String str=bf.readLine();
-			Stack<Character> s= new Stack<>();
-			
-			for(char ch : str.toCharArray()) {
-				if(ch=='(') {
-					s.push('(');
-				}else if(ch ==')') {
-					if(s.isEmpty()) {
-						isVPS=false;
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		while (true) {
+			boolean isVPS = true;
+			String str = bf.readLine();
+
+			if (str.charAt(0) == '.') {
+				break;
+			}
+
+			Stack<Character> s = new Stack<>();
+
+			for (char ch : str.toCharArray()) {
+				if (ch == '(' || ch == '[') {
+					s.push(ch);
+				} else if (ch == ')') {
+					if (s.isEmpty() || s.peek() == '[') {
+						isVPS = false;
 						break;
-					}else {
+					} else if (s.peek() == '(') {
 						s.pop();
 					}
-					
-					
+				} else if (ch == ']') {
+					if (s.isEmpty() || s.peek() == '(') {
+						isVPS = false;
+						break;
+					} else if (s.peek() == '[') {
+						s.pop();
+					}
 				}
-			}//for
-			
-			if(!s.isEmpty()) isVPS = false;
-			if(isVPS) {
-				System.out.println("YES");
-			}else {
-				System.out.println("NO");
 			}
-			
-		}//while
-		
-		
-		
-		
+
+			if (isVPS && s.isEmpty()) {
+				bw.write("yes" + "\n");
+			} else {
+				bw.write("no" + "\n");
+			}
+
+		} // while
+
+		bw.close();
+
 	}
 
 }
